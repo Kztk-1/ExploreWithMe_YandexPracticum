d68df5a6632e0be6b23a24a11e4ccb44ac15ab68
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-04-22 19:15:46 +0300

    feat: созданы DTO-классы UserDto, UserShortDto, NewUserRequest (по спецификации)

---
+++ explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/user/dto/NewUserRequest.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.explore_with_me.feature.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class NewUserRequest {
+    @NotBlank(message = "Name cannot be blank")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 250 characters")
+    private String name;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
+    @Size(max = 100, message = "Email must be up to 254 characters")
+    private String email;
+}
---
+++ explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/user/dto/UserShortDto.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.explore_with_me.feature.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserShortDto {
+    private Long id;
+    private String name;
+}
