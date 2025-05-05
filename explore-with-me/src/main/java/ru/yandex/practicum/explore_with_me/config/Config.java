619417be6854c292738caa32dfa5f3b5a2ceb467
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-05-05 16:07:34 +0300

    refactor: константы класса User вынесены в Config

--- explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/config/Config.java
+++ explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/config/Config.java
@@ -2,6 +2,9 @@ package ru.yandex.practicum.explore_with_me.config;
 
 public class Config {
 
+    public static final int USER_NAME_MAX_LENGTH = 50;
+    public static final int USER_EMAIL_MAX_LENGTH = 100;
+
     public static final String CONFLICT_EXCEPTION_MESSAGE = "Conflict";
 
 }
--- explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/user/model/User.java
+++ explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/user/model/User.java
@@ -2,6 +2,9 @@ package ru.yandex.practicum.explore_with_me.feature.user.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import ru.yandex.practicum.explore_with_me.config.Config;
+
+import java.time.LocalDateTime;
 
 @Table(name = "users")
 @NoArgsConstructor
@@ -14,9 +17,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = Config.USER_NAME_MAX_LENGTH)
     private String name;
 
-    @Column(unique = true, nullable = false, length = 100)
+    @Column(unique = true, nullable = false, length = Config.USER_EMAIL_MAX_LENGTH)
     private String email;
 }
