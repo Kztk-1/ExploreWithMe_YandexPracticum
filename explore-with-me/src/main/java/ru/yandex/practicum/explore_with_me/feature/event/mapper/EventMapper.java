c02169ec29bdbb62c426b017d1cdb3de2a82bf28
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-05-06 19:28:30 +0300

    feat: создан EventMapper (через MapStruct)

---
+++ explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/event/mapper/EventMapper.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.explore_with_me.feature.event.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.yandex.practicum.explore_with_me.feature.event.dto.EventFullDto;
+import ru.yandex.practicum.explore_with_me.feature.event.dto.NewEventDto;
+import ru.yandex.practicum.explore_with_me.feature.event.model.Event;
+import ru.yandex.practicum.explore_with_me.feature.user.mapper.UserMapper;
+
+@Mapper(componentModel = "spring",
+//        uses = {UserMapper.class, CategoryMapper.class, LocationMapper.class})
+        uses = {UserMapper.class})
+public interface EventMapper {
+
+    EventFullDto toDto(Event event);
+
+    // ВАЖНО!!! Category и Initiator будем делать в сервисе!!!
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "initiator", ignore = true)
+    @Mapping(target = "paid", source = "paid")
+    Event toEntity(NewEventDto dto);
+}
