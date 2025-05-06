ecfde99cfee9f5c5573f23b50365824ce132c96e
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-05-06 19:25:15 +0300

    feat: создан EventFullDto

---
+++ explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/event/dto/EventFullDto.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.explore_with_me.feature.event.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.yandex.practicum.explore_with_me.feature.category.model.Category;
+import ru.yandex.practicum.explore_with_me.feature.event.model.Location;
+import ru.yandex.practicum.explore_with_me.feature.user.dto.UserShortDto;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventFullDto {
+    private String annotation;
+    private Category category;
+    private Integer confirmedRequests;
+    private LocalDateTime createdOn;
+    private String description;
+    private LocalDateTime eventDate;
+    private Long id;
+    private UserShortDto initiator;
+    private Location location;
+    private Boolean paid;
+    private Integer participantLimit;
+    private LocalDateTime publishedOn;
+    private Boolean requestModeration;
+    private String state;
+    private String title;
+    private Long views;
+}
