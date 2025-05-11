74aa390c045fba5062e19f2dfc97f13e0e8b3258
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-05-11 21:07:55 +0300

    feat: в CategoryService-интерфейс добавлены admin-методы

--- explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/category/service/CategoryService.java
+++ explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/category/service/CategoryService.java
@@ -2,10 +2,16 @@ package ru.yandex.practicum.explore_with_me.feature.category.service;
 
 import org.springframework.data.domain.PageRequest;
 import ru.yandex.practicum.explore_with_me.feature.category.dto.CategoryDto;
+import ru.yandex.practicum.explore_with_me.feature.category.dto.NewCategoryDto;
+import ru.yandex.practicum.explore_with_me.feature.category.dto.UpdateCategoryDto;
 
 import java.util.List;
 
 public interface CategoryService {
     List<CategoryDto> getCategories(PageRequest pageRequest);
     CategoryDto getCategory(Long catId);
+
+    CategoryDto addCategory(NewCategoryDto dto);
+    void deleteCategory(Long catId);
+    CategoryDto updateCategory(Long catId, UpdateCategoryDto dto);
 }
