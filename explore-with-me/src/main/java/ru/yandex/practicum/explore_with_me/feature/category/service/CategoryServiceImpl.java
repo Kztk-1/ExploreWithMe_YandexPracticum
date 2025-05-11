7ce9a7738948327d995b392913476ca74cedc1fe
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-05-11 21:08:17 +0300

    feat: в CategoryServiceImpl реализованы admin-методы

--- explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/category/service/CategoryServiceImpl.java
+++ explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/category/service/CategoryServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.yandex.practicum.explore_with_me.exception.NotFoundException;
 import ru.yandex.practicum.explore_with_me.feature.category.dto.CategoryDto;
+import ru.yandex.practicum.explore_with_me.feature.category.dto.NewCategoryDto;
+import ru.yandex.practicum.explore_with_me.feature.category.dto.UpdateCategoryDto;
 import ru.yandex.practicum.explore_with_me.feature.category.mapper.CategoryMapper;
 import ru.yandex.practicum.explore_with_me.feature.category.model.Category;
 import ru.yandex.practicum.explore_with_me.feature.category.repository.CategoryRepository;
@@ -19,6 +21,8 @@ public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
     private final CategoryMapper categoryMapper;
+
+    // Public
 
     @Override
     @Transactional(readOnly = true)
@@ -38,5 +42,35 @@ public class CategoryServiceImpl implements CategoryService {
                         String.format("Category with id=%d not found", catId)
                 ));
         return categoryMapper.toDto(category);
+    }
+
+    // Admin
+
+    @Override
+    @Transactional
+    public CategoryDto addCategory(NewCategoryDto dto) {
+        Category category = new Category();
+        category.setName(dto.getName());
+        return categoryMapper.toDto(categoryRepository.save(category));
+    }
+
+    @Override
+    @Transactional
+    public void deleteCategory(Long catId) {
+        Integer id = catId.intValue();
+        if (!categoryRepository.existsById(id)) {
+            throw new NotFoundException(String.format("Category with id=%d not found", catId));
+        }
+        categoryRepository.deleteById(id);
+    }
+
+    @Override
+    @Transactional
+    public CategoryDto updateCategory(Long catId, UpdateCategoryDto dto) {
+        Integer id = catId.intValue();
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(String.format("Category with id=%d not found", catId)));
+        category.setName(dto.getName());
+        return categoryMapper.toDto(categoryRepository.save(category));
     }
 }
