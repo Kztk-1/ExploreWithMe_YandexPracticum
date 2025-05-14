749e97cb99c2c9d17a46c11b8c1c9256e15dfa96
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-05-14 13:19:20 +0300

    feat: в clear-data.sql добавлены новые таблицы events и category

--- explore-with-me/src/main/resources/clear-data.sql
+++ explore-with-me/src/main/resources/clear-data.sql
@@ -1,7 +1,12 @@
 USE explore_db;
 
+-- Отключаем проверку внешних ключей
 SET FOREIGN_KEY_CHECKS = 0;
 
+-- Очищаем основные таблицы
 TRUNCATE TABLE users;
+TRUNCATE TABLE events;
+TRUNCATE TABLE category;
 
+-- Включаем проверку внешних ключей обратно
 SET FOREIGN_KEY_CHECKS = 1;
