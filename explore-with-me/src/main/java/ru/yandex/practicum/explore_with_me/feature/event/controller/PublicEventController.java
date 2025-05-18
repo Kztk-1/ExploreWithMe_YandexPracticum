63aa5c36773d37d55b9de254fec2f2899a754264
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-05-19 00:43:28 +0300

    refactor: AdminCategoryController

---
+++ explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/event/controller/PublicEventController.java
@@ -0,0 +1,56 @@
+package ru.yandex.practicum.explore_with_me.feature.event.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.explore_with_me.feature.event.dto.EventFullDto;
+import ru.yandex.practicum.explore_with_me.feature.event.dto.EventShortDto;
+import ru.yandex.practicum.explore_with_me.feature.event.service.EventService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
+public class PublicEventController {
+
+    private final EventService eventService; // Предполагается наличие сервиса
+
+    @GetMapping
+    public List<EventShortDto> getPublicEvents(
+            @RequestParam(required = false) String text,
+            @RequestParam(required = false) List<Long> categories,
+            @RequestParam(required = false) Boolean paid,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeStart,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime rangeEnd,
+            @RequestParam(defaultValue = "false") Boolean onlyAvailable,
+            @RequestParam(required = false) String sort,
+            @RequestParam(defaultValue = "0") int from,
+            @RequestParam(defaultValue = "10") int size) {
+
+        int page = size / from;
+        Pageable pageable = PageRequest.of(page, size);
+        List<EventShortDto> events = eventService.getPublicEvents(
+                text, categories, paid, rangeStart, rangeEnd,
+                onlyAvailable, sort, pageable
+        );
+        return events;
+    }
+
+    @GetMapping("/{id}")
+    public List<EventShortDto> getEventById(@PathVariable Long userId,
+                                      @RequestParam(defaultValue = "0") int from,
+                                      @RequestParam(defaultValue = "10") int size) {
+
+        int page = size / from;
+        Pageable pageable = PageRequest.of(page, size);
+        List<EventShortDto> events = eventService.getEventsByUserId(userId, pageable);
+        return events;
+    }
+
+
+}
