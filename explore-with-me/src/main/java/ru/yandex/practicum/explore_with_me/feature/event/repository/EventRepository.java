20bc90dfa2912ab21f1ba493f7dfcec32b977f59
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-05-22 00:16:10 +0300

    feat: в EventRepository добавлено наследование еще и JpaSpecificationExecutor для создания методов продвинутой сортировки

--- explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/event/repository/EventRepository.java
+++ explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/event/repository/EventRepository.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.explore_with_me.feature.event.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.explore_with_me.feature.event.model.Event;
 
@@ -13,9 +13,7 @@ import java.util.Optional;
  * Репозиторий для работы с событиями.
  */
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long> {
-
-    Page<Event> findByInitiatorId(Long userId, Pageable pageable);
+public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecificationExecutor<Event> {
 
     /**
      * Поиск событий пользователя с пагинацией
