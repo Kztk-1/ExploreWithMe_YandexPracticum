71a98e82572e23d0c3d95cfa4be935e50e117041
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-05-22 00:27:18 +0300

    feat: в EventRepository созданы методы поиска в бд с умной сортировкой из JpaSpecificationExecutor

--- explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/event/repository/EventRepository.java
+++ explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/event/repository/EventRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.explore_with_me.feature.event.model.Event;
+import ru.yandex.practicum.explore_with_me.feature.event.model.EventState;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -24,5 +27,27 @@ public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecific
      * Поиск конкретного события пользователя
      */
     Optional<Event> findByIdAndInitiatorId(Long id, Long initiatorId);
+
+    Page<Event> findAllByInitiator_IdInAndStateInAndCategory_IdInAndEventDateBetween(
+            List<Long> initiatorIds,
+            List<EventState> states,
+            List<Long> categoryIds,
+            LocalDateTime start,
+            LocalDateTime end,
+            Pageable pageable
+    );
+
+    Page<Event> findByStateAndTitleContainingIgnoreCaseOrAnnotationContainingIgnoreCaseAndCategory_IdInAndPaidAndEventDateBetweenAndConfirmedRequestsLessThan(
+            EventState state,
+            String textForTitle,
+            String textForAnnotation,
+            List<Long> categoryIds,
+            Boolean paid,
+            LocalDateTime rangeStart,
+            LocalDateTime rangeEnd,
+            Integer maxConfirmedRequests,  // сюда вы передаёте participantLimit
+            Pageable pageable
+    );
+
 
 }
