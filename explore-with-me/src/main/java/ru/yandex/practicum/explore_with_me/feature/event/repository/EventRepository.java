a5744d4287f5773953982ccf4f710c13f797f046
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-06-05 22:23:15 +0300

    fix: глобальный фикс методов в EventRepository
    Запросы пришлось написать вручную, чтобы учитывать ситуации с null

--- explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/event/repository/EventRepository.java
+++ explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/event/repository/EventRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.explore_with_me.feature.event.model.Event;
@@ -29,6 +30,15 @@ public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecific
      */
     Optional<Event> findByIdAndInitiatorId(Long id, Long initiatorId);
 
+    @Query("""
+        SELECT e
+          FROM Event e
+         WHERE (:users IS NULL OR e.initiator.id IN :users)
+           AND (:states IS NULL OR e.state IN :states)
+           AND (:categories IS NULL OR e.category.id IN :categories)
+           AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart)
+           AND (:rangeEnd   IS NULL OR e.eventDate <= :rangeEnd)
+        """)
     Page<Event> findAllByAdminFilters(
             @Param("users")       List<Long> users,
             @Param("states")      List<EventState> states,
@@ -38,6 +48,15 @@ public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecific
             Pageable pageable
     );
 
+    @Query("""
+    SELECT e FROM Event e
+    WHERE e.state = 'PUBLISHED'
+    AND (:text IS NULL OR LOWER(e.annotation) LIKE LOWER(CONCAT('%', :text, '%')) OR LOWER(e.description) LIKE LOWER(CONCAT('%', :text, '%')))
+    AND (:paid IS NULL OR e.paid = :paid)
+    AND (:rangeStart IS NULL OR e.eventDate >= :rangeStart)
+    AND (:rangeEnd IS NULL OR e.eventDate <= :rangeEnd)
+    AND (:categories IS NULL OR e.category.id IN :categories)
+    """)
     Page<Event> findPublic(
             @Param("text") String text,
             @Param("categories") List<Long> categories,
