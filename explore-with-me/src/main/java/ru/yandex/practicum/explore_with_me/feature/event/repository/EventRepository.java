661ffb4bd06ec9477b074e54aa19f2d582eb34b2
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-06-05 22:21:56 +0300

    feat: в EventRepository созданы методы findAllByAdminFilters и findPublic

--- explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/event/repository/EventRepository.java
+++ explore-with-me/src/main/java/ru/yandex/practicum/explore_with_me/feature/event/repository/EventRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.explore_with_me.feature.event.model.Event;
 import ru.yandex.practicum.explore_with_me.feature.event.model.EventState;
@@ -28,26 +29,22 @@ public interface EventRepository extends JpaRepository<Event, Long>, JpaSpecific
      */
     Optional<Event> findByIdAndInitiatorId(Long id, Long initiatorId);
 
-    Page<Event> findAllByInitiator_IdInAndStateInAndCategory_IdInAndEventDateBetween(
-            List<Long> initiatorIds,
-            List<EventState> states,
-            List<Long> categoryIds,
-            LocalDateTime start,
-            LocalDateTime end,
+    Page<Event> findAllByAdminFilters(
+            @Param("users")       List<Long> users,
+            @Param("states")      List<EventState> states,
+            @Param("categories")  List<Long> categoryIds,
+            @Param("rangeStart")  LocalDateTime rangeStart,
+            @Param("rangeEnd")    LocalDateTime rangeEnd,
             Pageable pageable
     );
 
-    Page<Event> findByStateAndTitleContainingIgnoreCaseOrAnnotationContainingIgnoreCaseAndCategory_IdInAndPaidAndEventDateBetweenAndConfirmedRequestsLessThan(
-            EventState state,
-            String textForTitle,
-            String textForAnnotation,
-            List<Long> categoryIds,
-            Boolean paid,
-            LocalDateTime rangeStart,
-            LocalDateTime rangeEnd,
-            Integer maxConfirmedRequests,  // сюда вы передаёте participantLimit
+    Page<Event> findPublic(
+            @Param("text") String text,
+            @Param("categories") List<Long> categories,
+            @Param("paid") Boolean paid,
+            @Param("rangeStart") LocalDateTime rangeStart,
+            @Param("rangeEnd") LocalDateTime rangeEnd,
+            @Param("onlyAvailable") Boolean onlyAvailable,
             Pageable pageable
     );
-
-
 }
